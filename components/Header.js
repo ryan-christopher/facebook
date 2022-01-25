46bf9d6f67bc2eff63f2a4aefc48093c8c63498e
ryan-christopher <rechristopher99@gmail.com>
2022-01-25 00:26:14 -0500

    facebook clone git initialize

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+function Header() {
+    return (
+        <div>
+            <h1>Facebook</h1>
+
+            {/* Left */}
+            <div>
+                <Image />
+            </div>
+
+
+            {/* Center  */}
+
+            {/* Right  */}
+
+        </div>
+    )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  content: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
